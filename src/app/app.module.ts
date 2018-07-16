676e061bfb9206b39fa4f4eaf566458bb4bb55ea
Kanatbek <150103070@stu.sdu.edu.kz>
2018-07-16 16:08:46 +0600

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { CreateTodoComponent } from './todo/create-todo/create-todo.component';
+import {FormsModule} from '@angular/forms';
+import {BsDropdownModule, ModalModule, TooltipModule} from 'ngx-bootstrap';
+import {
+  AccordionModule,
+  ButtonModule,
+  DataTableModule,
+  PanelModule,
+  RadioButtonModule,
+  TabViewModule,
+  ToolbarModule
+} from 'primeng/primeng';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TableModule} from 'primeng/table';
+import {ToastrModule} from 'ngx-toastr';
+
+const appRoutes: Routes = [
+  {path: 'main', component: CreateTodoComponent},
+  {path: 'create', component: CreateTodoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTodoComponent
   ],
   imports: [
-    BrowserModule
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    DataTableModule,
+    TabViewModule,
+    AccordionModule,
+    TableModule,
+    PanelModule,
+    ButtonModule,
+    ToastrModule.forRoot(),
+    RadioButtonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/create-todo/create-todo.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from '../domain/Task';
+import {ToastrService} from 'ngx-toastr';
+
+@Component({
+  selector: 'app-create-todo',
+  templateUrl: './create-todo.component.html',
+  styleUrls: ['./create-todo.component.css']
+})
+export class CreateTodoComponent implements OnInit {
+
+  arrays = new Array<Task>();
+  array = new Task();
+  task: string;
+  title: string;
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.arrays = [];
+  }
+
+  addTask() {
+    if (this.validate()) {
+    this.array.title = this.title;
+    this.array.task = this.task;
+    this.arrays = [...this.arrays, this.array];
+    this.initValues();
+    this.toastr.success('Successfully added');
+    }
+  }
+
+  initValues() {
+    this.array = new Task();
+    this.task = '';
+    this.title = '';
+  }
+
+  validate() {
+    if (!this.title || this.title.length === 0) {
+      this.toastr.warning('Write title of task');
+      return;
+    }
+    if (!this.task || this.task.length === 0) {
+      this.toastr.warning('Write task to do');
+      return;
+    }
+    return true;
+  }
+}
